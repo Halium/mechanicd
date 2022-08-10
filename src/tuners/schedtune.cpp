bf93a2c9d69ea54ccdecf2aa4b2665badfe4a019
Alfred Neumayer <dev.beidl@gmail.com>
2022-08-10 22:11:17 +0200

    tuners: Adjust schedtune.init_task_load_pct as well
    Found on the JingPad and has a very positive impact on the
    overall performance of the tablet.

--- src/tuners/schedtune.cpp
+++ src/tuners/schedtune.cpp
@@ -44,18 +44,34 @@ void SchedTuneTuner::tunePid(pid_t pid)
         if (cgroupParts[1] != QStringLiteral("schedtune"))
             continue;
 
-        const QString booster = QStringLiteral("/sys/fs/cgroup/schedtune/%1/schedtune.boost").arg(cgroupParts[2]);
-        QFile boosterFile(booster);
-
-        if (!boosterFile.exists())
-            continue;
-
-        if (!boosterFile.open(QFile::WriteOnly))
-            continue;
-
+        // Main cgroup control file for adjusting frequency boosting
         {
+            const QString booster = QStringLiteral("/sys/fs/cgroup/schedtune/%1/schedtune.boost").arg(cgroupParts[2]);
+            QFile boosterFile(booster);
+
+            if (!boosterFile.exists())
+                continue;
+
+            if (!boosterFile.open(QFile::WriteOnly))
+                continue;
+
             QTextStream textStream(&boosterFile);
             textStream << "20";
+        }
+
+        // Optional schedtune value found on the JingPad
+        {
+            const QString load_pct = QStringLiteral("/sys/fs/cgroup/schedtune/%1/schedtune.init_task_load_pct").arg(cgroupParts[2]);
+            QFile loadPctFile(load_pct);
+
+            if (!loadPctFile.exists())
+                continue;
+
+            if (!loadPctFile.open(QFile::WriteOnly))
+                continue;
+
+            QTextStream textStream(&loadPctFile);
+            textStream << "45";
         }
     }
 }
